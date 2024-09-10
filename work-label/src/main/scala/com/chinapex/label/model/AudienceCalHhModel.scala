91161a0f69f4fcc7f84751967f49141c0b221ce4
zhaoyijie <747897928@qq.com>
2024-09-10 20:01:26 +0800

    delete two scala file

--- work-label/src/main/scala/com/chinapex/label/model/AudienceCalHhModel.scala
+++
@@ -1,8 +0,0 @@
-package com.chinapex.label.model
-
-/**
- *
- * @param relation                  和上一组规则之间的关系 0:and  1:or
- * @param audienceCalParams         计算参数
- */
-case class AudienceCalHhModel(relation : Int, audienceCalParams : List[AudienceCalParamModel])
--- work-label/src/main/scala/com/chinapex/label/model/AudienceCalParamModel.scala
+++
@@ -1,9 +0,0 @@
-package com.chinapex.label.model
-
-/**
- *
- * @param tableName    执行sql对应的数据表
- * @param sql          执行sql
- * @param relation     和上一组规则之间的关系 0:and  1:or
- */
-case class AudienceCalParamModel(tableName : String, sql : String, relation : Int)
